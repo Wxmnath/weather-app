a88901508728920f431d94573995f3e4a99d3cfd
Wxmnath <Wxmnath@aol.com>
2022-03-18 15:30:25 +0000

    ForecastSummary test created

--- src/Tests/App.test.js
+++ src/Tests/App.test.js
@@ -1,7 +1,7 @@
 import { render, screen } from "@testing-library/react";
 import App from "../Components/App";
 
-test("renders learn react link", () => {
+xtest("renders learn react link", () => {
   render(<App />);
   const linkElement = screen.getByText(/Weather App/i);
   expect(linkElement).toBeInTheDocument();
---
+++ src/Tests/components/ForecastSummary.test.js
@@ -0,0 +1,26 @@
+import React from "react";
+import { render } from "@testing-library/react";
+import ForecastSummary from "../../Components/ForecastSummary";
+
+describe("ForecastSummary", () => {
+  const validProps = {
+    date: 1111111,
+    description: "Stub description",
+    icon: "stubIcon",
+    temperature: {
+      min: 12,
+      max: 22,
+    },
+  };
+  it("renders correctly", () => {
+    const { asFragment } = render(
+      <ForecastSummary
+        date={validProps.date}
+        description={validProps.description}
+        icon={validProps.icon}
+        temperature={validProps.temperature}
+      />
+    );
+    expect(asFragment()).toMatchSnapshot();
+  });
+});

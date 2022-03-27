d5c0f7ffb0850064e53f49eb81a0a073156fb9bc
Wxmnath <Wxmnath@aol.com>
2022-03-27 11:00:31 +0100

    Application now able to search and return weather for different cities

--- src/Components/App.js
+++ src/Components/App.js
@@ -4,27 +4,37 @@ import ForecastSummaries from "./ForecastSummaries";
 import ForecastDetails from "./ForecastDetails";
 import getForecast from "./getForecast";
 import "../Styles/App.css";
+import SearchForm from "./SerchForm";
 
 function App() {
   const [forecasts, setForecasts] = useState([]);
   const [location, setLocation] = useState({ city: "", country: "" });
   const [selectedDate, setSelectedDate] = useState(0);
-
+  const [searchText, setSearchText] = useState("");
   const selectedForecast = forecasts.find(
     (forecast) => forecast.date === selectedDate
   );
+
+  const handleCitySearch = () => {
+    getForecast(searchText, setSelectedDate, setForecasts, setLocation);
+  };
 
   const handleForecastSelect = (date) => {
     setSelectedDate(date);
   };
 
   useEffect(() => {
-    getForecast(setSelectedDate, setForecasts, setLocation);
-  }, []);
+    getForecast(searchText, setSelectedDate, setForecasts, setLocation);
+  }, [searchText]);
 
   return (
     <div className="weather-app">
       <LocationDetails city={location.city} country={location.country} />
+      <SearchForm
+        searchText={searchText}
+        setSearchText={setSearchText}
+        onSubmit={handleCitySearch}
+      />
       <ForecastSummaries
         forecasts={forecasts}
         onForecastSelect={handleForecastSelect}
